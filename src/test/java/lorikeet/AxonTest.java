f66f5ad674c62a9dd4ce1a7ca39392d4175d7fc0
Blair Harris <blairehh@gmail>
2021-05-16 19:19:16 +0200

    modify

--- src/test/java/lorikeet/AxonTest.java
+++ src/test/java/lorikeet/AxonTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 public class AxonTest {
     @Test
     public void test() {
-        final Axon<App> axon = new DefaultAxon<>(new App(18));
+        final Axon<App> axon = new DefaultAxon<>(new App(21));
         System.out.println(axon.run(new MyAction()));
     }
 }
